3e030da851393d38b12b71d8537c7959605a8fc2
fiteclub <mattf255@yahoo.com>
2020-10-13 22:54:57 -0400

    Refactor switch (DRY)

--- ref.sh
+++ ref.sh
@@ -5,28 +5,25 @@ txt_path="$(dirname "$0")/txt"
 case "$1" in 
 
   -p | p | --pry | -pry | pry )
-    ccat --bg=dark $txt_path/pry.txt
+    type=pry
     ;;
 
   -r | r | --rails | -rails | rails )
-    ccat --bg=dark $txt_path/rails.txt
+    type=rails
     ;;
 
   -v | v | --vi | -vi | vi | --vii | -vii | vii | --vim | -vim | vim )
-    ccat --bg=dark $txt_path/vi.txt
+    type=vim
     ;;
 
   -c | c | --code | -code | code | --vscode | -vscode | vscode | --vsc | -vsc | vsc )
-    ccat --bg=dark $txt_path/vscode.txt
+    type=vscode
     ;;
 
   ## Easter Eggs
-  kitteh )
-    sed -n 4,31p $txt_path/wtflol.txt 
-    ;;
 
   doh )
-    sed -n 35,96p $txt_path/wtflol.txt 
+    type=wtflol
     ;;
 
   ##
@@ -36,3 +33,5 @@ case "$1" in
     echo "  -p, --pry, -r, --rails, -v, --vii, -c, --code, --vscode"
     ;;
 esac
+
+ccat --bg=dark $txt_path/$type.txt
